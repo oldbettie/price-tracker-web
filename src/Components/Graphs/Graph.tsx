5fb807abbfe3578e43c8fc2db89120716947877c
jjeaton <oldbettiegaming@gmail.com>
2023-05-24 17:06:30 +0800

    adds basic implementation for displaying an array of product prices and discounts

--- src/Components/Graphs/Graph.tsx
+++ src/Components/Graphs/Graph.tsx
@@ -8,23 +8,31 @@ export interface GraphProps {
 }
 
 export function Graph({ name }: GraphProps): JSX.Element {
-    const [productInfo, setProductInfo] = useState({})
+    const [productInfo, setProductInfo] = useState<Product[]>([])
+    const [error, setError] = useState<string>()
 
     useEffect(() => {
         async function getProductData(){
             try {
                 const res: Response = await fetch('http://localhost:8080/item/270_Free-Standing_Awning')
                 const data = await res.json();
-
+                const productArray: Product[] = []
+                data.forEach((item: any) => {
+                    productArray.push({
+                        name: item.product_name,
+                        price: item.price,
+                        date: item.insert_date,
+                        discount: item.product_discount
+                    })
+                })
                 if (!res.ok) {
-                    // get error message from body or default to response statusText
                     const error = (data && data.message) || res.statusText;
                     return Promise.reject(error);
                 }
                 console.log(data)
-                setProductInfo(data)
+                setProductInfo(productArray)
             } catch (error: any) {
-                setProductInfo( {errorMessage: error.toString()});
+                setError(error.message)
                 console.error('There was an error!', error);
             }
         }
@@ -36,6 +44,22 @@ export function Graph({ name }: GraphProps): JSX.Element {
 
         <div>
             <h2>{name}</h2>
+            { productInfo.map((product: Product, i) => {
+                return (
+                    <div key={i}>
+                        <p>{product.price}</p>
+                        <p>{product.discount}</p>
+                        <p>{product.date}</p>
+                    </div>
+                )
+            }) }
         </div>
     )
+}
+
+interface Product {
+    name: string
+    price: number,
+    date: string,
+    discount?: string,
 }
