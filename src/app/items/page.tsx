15cdf070951a9ac6b91f9f04ecf9c7d0247873ca
oldbettie <oldbettiegaming@gmail.com>
2023-05-29 12:19:54 +0800

    add basic graph component and page

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -1,19 +1,19 @@
 'use client'
 
-import {Navigation} from "@/Components/Navigation/Navigation";
 import React from "react";
 import genericStyles from "@/app/page.module.css";
 import {Graph} from "@/Components/Graphs/Graph";
+import {Navigation} from "@/Components/Navigation/Navigation";
 
 export default function Items(): JSX.Element {
     return (
         <main>
-            <div className={genericStyles.backgroundImage}>
-                <div className={genericStyles.backgroundFilter}>
-                    <div className={genericStyles.container}>
-                        <Navigation/>
-                        <Graph name="270 awning" />
-                    </div>
+            <div className={genericStyles.backgroundImage} />
+            <div className={genericStyles.backgroundFilter} />
+            <Navigation/>
+            <div className={genericStyles.container}>
+                <div className={genericStyles.contentContainer}>
+                    <Graph inName="270_Free-Standing_Awning" />
                 </div>
             </div>
         </main>
