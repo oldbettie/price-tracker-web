047a795ecbee30e274de890e0146b6cc7668b130
oldbettie <oldbettiegaming@gmail.com>
2023-05-29 20:45:13 +0800

    add logo and adjust css to use the color scheme

--- src/Components/Navigation/Navigation.tsx
+++ src/Components/Navigation/Navigation.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import styles from './Navigation.module.css'
 import {Button} from "@/Components/Button/Button";
+import Image from "next/image";
 
 export function Navigation(): JSX.Element {
     return (
         <div className={styles.container}>
-            <h1>Price Mates</h1>
+            <div className={styles.imageContainer}>
+                <Image src="/priceMates.png" alt="Price Mates logo" width={220} height={60} className={styles.logo}/>
+            </div>
             <div className={styles.buttonContainer}>
                 <Button content="About" path={"/about"} />
                 <Button content="Contact Us" path={"/contact-us"}/>
