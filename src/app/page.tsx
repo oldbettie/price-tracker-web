94802adbe72e94d5cff479f43cfd8718f11a5cd9
oldbettie <oldbettiegaming@gmail.com>
2023-05-29 20:57:57 +0800

    refactor components to follow new colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,12 +11,15 @@ export default function Home() {
     <PageContent>
         <h2>Find the best deals on 4wd parts and camping equipment.</h2>
         <p>
+            Price Mates is currently under development. to sign up for early offers sign up for our newsletter <a
+            href="/contact-us">here.</a>
+        </p>
+        <p>
             The goal of this site is to provide a place for people to find the best deals on 4wd parts and camping equipment.
             With the increased cost of living more and more people are waiting patiently for a good sale to come along before
             purchasing the parts they need. This site will allow you to track the price of the parts you need and notify you
             when the price drops below a certain threshold.
         </p>
-        <br/>
     </PageContent>
   )
 }
