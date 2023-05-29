7afa252d1876ace1d43cf0e15d83b23360708d3b
oldbettie <oldbettiegaming@gmail.com>
2023-05-29 21:15:59 +0800

    refactor homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {PageContent} from "@/Components/page/PageContent";
-
+import Image from "next/image"
+import styles from "./home.module.css"
 export const metadata = {
     title: "Price Mates",
     description: "find and compare prices on camping and 4wd equipment"
@@ -16,9 +17,23 @@ export default function Home() {
         </p>
         <p>
             The goal of this site is to provide a place for people to find the best deals on 4wd parts and camping equipment.
+        </p>
+        <p>
             With the increased cost of living more and more people are waiting patiently for a good sale to come along before
             purchasing the parts they need. This site will allow you to track the price of the parts you need and notify you
             when the price drops below a certain threshold.
+        </p>
+        <br/>
+        <p>Early PROTOTYPES look something like this.</p>
+        <br/>
+        <div className={styles.imageContainer}>
+            <Image src="/item-prices.png" alt="graph sample photo" fill={true} />
+        </div>
+        <p>My main focus is to provide easy to understand price history and the most useful data to that goes with it.</p>
+        <br/>
+        <p>
+            Future iterations will go into more detail and you will be able to check out more product info, these things take time,
+            it also takes time to collect a large dataset to use.
         </p>
     </PageContent>
   )
