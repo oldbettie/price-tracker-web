83eb12596cab8dd75c16c308b73277f69871bf46
oldbettie <oldbettiegaming@gmail.com>
2023-06-03 12:03:22 +0800

    remove debugger

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {PageContent} from "@/Components/page/PageContent";
 import Link from "next/link";
 import {productRoutes} from "@/app/items/[id]/products";
+import {notFound} from "next/navigation";
 
 export const metadata = {
     title: "Items - Price Mates",
@@ -9,18 +10,21 @@ export const metadata = {
 }
 
 export default function Items(): JSX.Element {
-    debugger
+    const routes = productRoutes
+
+    if (!routes) notFound()
+
     return (
         <PageContent>
             <h2>All products we currently track</h2>
             <p>If a product you like is not on the list below, please let us know <Link href={"/contact-us"}>here!</Link></p>
             <br/>
-            {productRoutes.map((link, index) => {
+            {routes.map((link, index) => {
                 return (
                     <div key={index}>
                         <Link href={`/items/${link.link}`}><h2>{link.name}</h2></Link>
                     </div>
-                    )}
+                )}
             )}
         </PageContent>
     );
