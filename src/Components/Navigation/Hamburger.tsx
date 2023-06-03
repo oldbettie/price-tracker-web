206a8c01b5571cab5f01923906d942e821e96dae
oldbettie <oldbettiegaming@gmail.com>
2023-06-03 18:49:52 +0800

    adds basic hamburger for mobile

---
+++ src/Components/Navigation/Hamburger.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import {FiMenu, FiX} from 'react-icons/fi';
+import {Button} from "@/Components/Button/Button";
+import styles from "./hamburger.module.css"
+import {UserConfig} from "@/lib/config/UserConfig";
+
+export const HamburgerMenu = () => {
+    const [isOpen, setIsOpen] = useState(true);
+
+    function toggleMenu() {
+        setIsOpen(!isOpen);
+    }
+
+    return (
+        <div >
+            {isOpen ?
+            <div className={styles.hamburgerIcon}>
+                <FiMenu size={35} onClick={toggleMenu} />
+            </div> :
+            <div className={styles.hamburgerContent}>
+                <FiX size={50} onClick={toggleMenu}/>
+                <div cclassName={styles.buttonContainer}>
+                    <Button content="Home" path={"/"} click={toggleMenu} />
+                    {UserConfig.ITEMS_FEATURE && <Button content="Items" path={"/items"} click={toggleMenu} />}
+                    <Button content="About" path={"/about"} click={toggleMenu} />
+                    <Button content="Contact Us" path={"/contact-us"} click={toggleMenu} />
+                </div>
+            </div>
+            }
+        </div>
+    )
+}
+
