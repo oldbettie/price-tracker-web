5a3b4997a102ff7a34d8c5c823f6b6621e75e750
oldbettie <oldbettiegaming@gmail.com>
2023-06-03 12:11:14 +0800

    add test page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {PageContent} from "@/Components/page/PageContent";
+
+export const metadata = {
+    title: "Dashboard - Price Mates",
+    description: "find and compare prices on camping and 4wd equipment"
+}
+
+export default function About(): JSX.Element {
+    return (
+        <PageContent>
+            <h2>- Dashboard -</h2>
+        </PageContent>
+    );
+}
