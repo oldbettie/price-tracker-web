c73a37d581278ddbc7901b80ab5e39dc9ab86659
oldbettie <oldbettiegaming@gmail.com>
2023-06-16 16:16:37 +0800

    add axios and supabase cloud logic

--- src/lib/config/FeatureConfig.ts
+++ src/lib/config/FeatureConfig.ts
@@ -1,14 +1,19 @@
 export interface FeatureConfigI {
     ITEMS_FEATURE: boolean
     API_URL: string
+    SUPABASE_URL: string
 }
 
 export const localConfig: FeatureConfigI = {
     API_URL: "http://localhost:8080",
     ITEMS_FEATURE: true,
+    SUPABASE_URL: "http://localhost:54321"
 }
 
 export const productionConfig: FeatureConfigI = {
-    API_URL: `${process.env.NEXT_PUBLIC_API_URL}`,
+    API_URL: process.env.NEXT_PUBLIC_API_URL ? process.env.NEXT_PUBLIC_API_URL : "http://localhost:8080",
     ITEMS_FEATURE: false,
+    SUPABASE_URL: `${process.env.NEXT_PUBLIC_SUPABASE_URL}`
 }
+
+
